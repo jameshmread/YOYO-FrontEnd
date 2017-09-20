b6145a34c38fd12d4e75b362aba6991ac0c94874
jameshmread <jamesread117@yahoo.co.uk>
2017-09-21 00:43:14 +0100

    added required imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import "reflect-metadata";
 import "zone.js/dist/zone";
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { SelectButtonModule } from "primeng/primeng";
 
 import { AppComponent } from "./app.component";
 @NgModule({
   declarations: [
     AppComponent
   ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    SelectButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
