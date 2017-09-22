763c0bd9ce32a2604f5ad592ea041357257d318b
tomcatooo <tomasdrobinson@gmail.com>
2017-09-22 11:13:30 +0100

    added json dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,26 @@ import "reflect-metadata";
 import "zone.js/dist/zone";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from '@angular/common/http';
+import { JsonFetcherService } from './jsonFetcherService/JsonFetcherService';
+import { JsonFetcherExample } from './jsonFetcherExample/JsonFetcherExample';
+import { LineChart } from './charts/LineChart';
+
 
 import { AppComponent } from "./app.component";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JsonFetcherExample,
+    LineChart
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+   JsonFetcherService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
